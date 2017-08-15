37142f573502a1d6d626d3acbdbdfbc00a7aa9ad
Paul Breslin <paul.breslin@outlook.com>
2017-08-15 01:12:46 +0100

    Updates appheader

--- src/components/appHeader/appHeader.js
+++ src/components/appHeader/appHeader.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './appHeader.css';
 import LogoImg from '../../assets/logo.svg';
 import MenuIcon from '../../assets/hamburger.svg';
 
-const AppHeader = () => {
-	return(
-		<header className="app-header">
-			{/*
-				logo
-				Deliveroo
-				menu
-				(close icon)
-			*/}
-			<a href="/" className="mobile-logo">
-				<img src={LogoImg} alt="Deliveroo Logo" />
-			</a>
-			<img src={MenuIcon} alt="Menu" className="menu-icon" />
-		</header>
-	)
+class AppHeader extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			showMobileNav: false
+		};
+	}
+
+	handleMenuClick = () => {
+		const {showMobileNav} = this.state;
+		this.setState({
+			showMobileNav: !showMobileNav
+		});
+	}
+
+	render() {
+		return(
+			<header className={'app-header ' + (this.state.showMobileNav ? 'show-mobile' : '')}>
+				<a href="/" className="mobile-logo">
+					<img src={LogoImg} alt="Deliveroo Logo" />
+				</a>
+				<img
+					src={MenuIcon}
+					alt="Menu"
+					className="menu-icon"
+					onClick={this.handleMenuClick} />
+				<nav className="app-nav">
+					<a href="">Home</a>
+					<a href="">Search</a>
+					<a href="">£8.55</a>
+					<a href="">Simon Rohrbach</a>
+				</nav>
+			</header>
+		)
+	}
 }
 
 export default AppHeader;
